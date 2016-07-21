c3e17261e0573615aca157f51fadee30b89889d2
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-07-20 21:38:20 -0800

    Initial Commit

---
+++ ConfigurationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfigurationTest
+//
+//  Created by 齋藤緒 on 2016/07/20.
+//  Copyright © 2016年 TestOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
